3a9367bb5d9e355e682c1d6846a775e1e13f6ab6
George Gastaldi <gegastaldi@gmail.com>
2026-04-15 19:49:29 -0300

    Update ci-prerequisites.sh
    Fixes the errors in the logs: 
    
    Error response from daemon: No such image: node:10
    Error response from daemon: No such image: node:12
    Error response from daemon: No such image: mcr.microsoft.com/azure-pipelines/node8-typescript:latest

--- ci-prerequisites.sh
+++ ci-prerequisites.sh
@@ -1,4 +1,3 @@
 # Reclaim disk space
-time docker rmi node:10 node:12 mcr.microsoft.com/azure-pipelines/node8-typescript:latest
 time sudo rm -rf /usr/share/dotnet
 time sudo rm -rf /usr/share/swift
